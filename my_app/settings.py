ff86d71807b6a7ad503884c34ab082847b8a5044
Amir <pangan@gmail.com>
2019-03-22 10:04:09 +0100

    Add files via upload

---
+++ my_app/settings.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Amir Mofakhar <amir@mofakhar.info>
+Python Version: 3.7
+"""
+
+CELERY_BROKER_FOLDER = '/Users/amir/dev/my_test/broker'
+CELERY_BACKEND_FOLDER = '/Users/amir/dev/my_test/backend'
