9feca99efd81ee404c23110a62d78d1b064aa0d6
Amir <pangan@gmail.com>
2019-03-22 10:03:41 +0100

    Delete settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Author: Amir Mofakhar <amir@mofakhar.info>
-Python Version: 3.7
-"""
-
-CELERY_BROKER_FOLDER = '/Users/amir/dev/my_test/broker'
-CELERY_BACKEND_FOLDER = '/Users/amir/dev/my_test/backend'
